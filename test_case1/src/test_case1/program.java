65b834990d4f2520d5dc8e4fb1fcfb360bd53200
JANANI <JANANI@platform.wondershare.com>
2023-07-24 14:50:29 +0530

    Array program

---
+++ test_case1/src/test_case1/program.java
@@ -0,0 +1,17 @@
+package test_case1;
+
+public class program {
+
+	public static void main(String[] args) {
+		int[] age = {12,4,5};
+		System.out.println("Using For Loop:");
+		for(int i = 0; i < age.length; i++) {
+			System.out.println(age[i]);
+		}
+
+	}
+
+}
+		
+
+		
